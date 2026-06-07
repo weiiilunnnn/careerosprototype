d59981491076f4f3fce15fb672cd6ec7813ec2ef
weiiilunnnn <lunweii1025@gmail.com>
2026-06-07 14:31:05 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
 import NavbarWrapper from "@/components/NavbarWrapper";
 import "./globals.css";
 
@@ -29,7 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <NavbarWrapper />
+        <Suspense fallback={null}>
+          <NavbarWrapper />
+        </Suspense>
+
         {children}
       </body>
     </html>
